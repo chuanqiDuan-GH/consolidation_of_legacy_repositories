52b510c8c6ef7bd3941338778370bff572797175
chuanqiDuan-GH <whisper.xd@qq.com>
2016-11-27 13:53:08 +0800

    [feat](1-w_q/):内核并发处理-等待队列

---
+++ tempdir/driver/8-blk_nonblk_io/1-wait_queue/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define SIZE 64
+
+int main()
+{
+    int fd;
+    int ret;
+    char buf[SIZE] = {0};
+    pid_t pid;
+
+    fd = open("/dev/myDev0", O_RDWR);
+    if( -1==fd ) {
+	perror("open");
+	return -1;
+    }
+
+    printf("open successfully!\n");
+
+    read(fd, buf, SIZE);
+
+    printf("read:%s\n", buf);
+    
+    close(fd);
+
+    return 0;
+}
