8f5fddc690d0d24d0f5279b87d05d273e9e0c32e
chuanqiDuan-GH <whisper.xd@qq.com>
2016-11-28 21:12:16 +0800

    [feat](1-platform/):linux设备模型基本框架

---
+++ tempdir/driver/9-platform_model/1-platform/cdev.c
@@ -0,0 +1,62 @@
+#include <linux/kernel.h>
+#include <linux/cdev.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/device.h>
+#include <linux/platform_device.h>
+
+void my_release(struct device *dev)
+{
+    printk("%s %d\n", __func__, __LINE__);
+}
+
+struct platform_device myDevice = 
+{
+    .name = "myDev",
+    .dev = 
+    {
+	.release = my_release, 
+    },
+};
+
+int my_probe(struct platform_device *dev)
+{
+    printk("%s %d\n", __func__, __LINE__);
+    return 0;
+}
+
+int my_remove(struct platform_device *dev)
+{
+    printk("%s %d\n", __func__, __LINE__);
+    return 0;
+}
+
+struct platform_driver myDriver =
+{
+    .probe = my_probe,
+    .remove = my_remove,
+    .driver = 
+    {
+	.name = "myDev"	, 
+    },
+};
+
+static int __init my_init(void)
+{
+    platform_device_register(&myDevice);
+    platform_driver_register(&myDriver);
+
+    return 0;
+}
+
+static void __exit my_exit(void)
+{
+    platform_device_unregister(&myDevice);
+    platform_driver_unregister(&myDriver);
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
